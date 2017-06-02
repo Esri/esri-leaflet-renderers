57ec43bdef30e30883974f90d9ca6079e5c70ebb
jgravois <jgravois@esri.com>
2017-06-02 10:29:17 -0700

    bugless lint fix

--- src/Symbols/Symbol.js
+++ src/Symbols/Symbol.js
@@ -121,7 +121,7 @@ export var Symbol = L.Class.extend({
             // weights to each of the rgba colors and adding them together
             var interpolatedColor = [];
             for (var j = 0; j < 4; j++) {
-              interpolatedColor[j] = (Math.round(lowerBoundColor[j] * lowerBoundColorWeight) + (upperBoundColor[j] * upperBoundColorWeight));
+              interpolatedColor[j] = Math.round((lowerBoundColor[j] * lowerBoundColorWeight) + (upperBoundColor[j] * upperBoundColorWeight));
             }
             return interpolatedColor;
           } else {
